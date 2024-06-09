f7996aeaca4a1d6853f0e5dc8e55294a41eaa1a6
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-09 02:30:57 -0500

    Create JwtService, add constructor

---
+++ AuthenticationJWT/Services/JwtService.cs
@@ -0,0 +1,16 @@
+﻿namespace AuthenticationJWT.Services
+{
+    public class JwtService
+    {
+        private readonly string _secretKey;
+        private readonly string _issuer;
+        private readonly string _audience;
+
+        public JwtService(string secretKey, string issuer, string audience)
+        {
+            _secretKey = secretKey;
+            _issuer = issuer;
+            _audience = audience;
+        }
+    }
+}
