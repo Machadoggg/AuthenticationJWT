e40205ff2ce23ba3f0d63fefa5fbf2d0ff4bac46
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-09 09:21:40 -0500

    Add Login method

--- AuthenticationJWT/Controllers/AuthController.cs
+++ AuthenticationJWT/Controllers/AuthController.cs
@@ -13,5 +13,23 @@ namespace AuthenticationJWT.Controllers
         {
             _jwtService = jwtService;
         }
+
+
+        [HttpPost("Login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            if (request.Username == "admin" && request.Password == "password") 
+            {
+                var token = _jwtService.GenerateToken(request.Username);
+                return Ok(new { token });
+            }
+            return Unauthorized();
+        }
+    }
+
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
