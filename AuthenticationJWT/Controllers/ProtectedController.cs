8d7194f44b77f9b31915d1ab55de8699b774d729
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-09 09:39:44 -0500

    Controller with a Protected action

--- AuthenticationJWT/Controllers/ProtectedController.cs
+++ AuthenticationJWT/Controllers/ProtectedController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace AuthenticationJWT.Controllers
 {
+    
     [Route("api/[controller]")]
     [ApiController]
     public class ProtectedController : ControllerBase
     {
+        [Authorize]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // Protected action, only accessible with a valid token
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            return Ok($"Hello {username}! This is a protected resource.");
+        }
     }
 }
